14c4ef3e790317c4fe68041fa7f0ebc56c987971
Andrew Giel <agiel@hearsaycorp.com>
2014-06-18 11:12:11 -0700

    added game module

--- src/test.py
+++ src/test.py
@@ -1,3 +1,13 @@
 import getWords 
+import game 
+
 board = [['a', 'b', 'c', 'd', 'e']]
-print getWords.getWords(board)
+state = game.Game(board)
+state.setColor(0, 0, game.MAX_COLOR_UNLOCKED)
+state.setColor(0, 1, game.MAX_COLOR_UNLOCKED)
+state.setColor(1, 0, game.MAX_COLOR_UNLOCKED)
+state.lockColors()
+print state.getColor(0, 0), 'should be 2'
+state.setColor(0, 1, game.MIN_COLOR_UNLOCKED)
+state.lockColors()
+print state.getColor(0, 0), 'should be 1'
